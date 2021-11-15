d9f460f6e95ccedb31860c429504311d086fb0bb
Lena Bartell <lena@altana.ai>
2021-11-15 09:02:14 -0500

    update port

--- setup_nginx.sh
+++ setup_nginx.sh
@@ -1,3 +1,5 @@
+# https://medium.com/techfront/step-by-step-visual-guide-on-deploying-a-flask-application-on-aws-ec2-8e3e8b82c4f7
+# https://stackoverflow.com/questions/17413526/nginx-missing-sites-available-directory
 sudo amazon-linux-extras install nginx1
 sudo systemctl start nginx
 sudo systemctl enable nginx
