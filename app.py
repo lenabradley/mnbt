f966563538cec335e0ea068794f862b4d19d7ad7
Lena Bartell <lena@altana.ai>
2021-11-20 17:30:53 -0500

    add emojis

--- app.py
+++ app.py
@@ -18,12 +18,13 @@ def index():
     <html>
     <body style="background-color:rosybrown;">
     <center>
+    <br><br>
     <i>Are you ready for</i>
     <h2>MONDAY NIGHT BEFORE THANKSGIVING?</h2>
     <br><br>
     Today is {date.format("dddd MMMM Do, YYYY")}, {message}
     <br><br>
-    <p style="font-size:50">&#129411; &#127810; &#127792; &#127809; &#129383; &#127867;</p>
+    <p style="font-size:50px">&#129411; &#127810; &#127792; &#127809; &#129383; &#127867; &#10084;</p>
     </center>
     </body>
     </html>
