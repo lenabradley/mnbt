a263802c60e4cd1a83bcb7cb3f7e2feed70d4808
Lena Bartell <lena@altana.ai>
2021-11-20 17:23:19 -0500

    typo

--- bin/run_app.sh
+++ bin/run_app.sh
@@ -1,2 +1,2 @@
 # https://medium.com/techfront/step-by-step-visual-guide-on-deploying-a-flask-application-on-aws-ec2-8e3e8b82c4f7
-nano ./venv/bin/gunicorn --bind 0.0.0.0:80 app:app
+sudo ./venv/bin/gunicorn --bind 0.0.0.0:80 app:app
