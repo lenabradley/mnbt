6a91ece89b0f08e0ecbaff8c9bdaf3c76824ec5e
Lena Bartell <lena@altana.ai>
2021-11-21 10:46:04 -0500

    fixing gif

--- lib/html.py
+++ lib/html.py
@@ -1,16 +1,14 @@
 import pendulum
 import flask
-from lib import mnbt
+from lib import mnbt, defaults
 
 
 def generate_index(date: pendulum.Date) -> str:
     """Generate index page html"""
     message = mnbt.hello_thread(date)
-    # celebration_gif = \
-    #     f'<br><br><img src="{defaults.celebration_gif_path}" /><br><br>' \
-    #     if mnbt.is_thanksgiving_week(date) else ""
-    gif_url = flask.url_for('static', filename="celebration.gif")
-    celebration_gif = f'<br><br><img src="{gif_url}" /><br><br>'
+    celebration_gif = \
+        f'<br><img src="{defaults.celebration_gif_path}" /><br>' \
+            if mnbt.is_thanksgiving_week(date) else ""
 
     return \
         f"""
@@ -23,9 +21,9 @@ def generate_index(date: pendulum.Date) -> str:
                     <h2>MONDAY NIGHT BEFORE THANKSGIVING?</h2>
                     <br><br>
                     Today is {date.format("dddd MMMM Do, YYYY")}, {message}
-                    <br><br>
-                    <p style="font-size:50px">&#129411; &#127810; &#127792; &#127809; &#129383; &#127867; &#10084;</p>
                     {celebration_gif}
+                    <p style="font-size:50px">&#129411; &#127810; &#127792; &#127809; &#129383; &#127867; &#10084;</p>
+                    <br><br>
                 </center>
             </body>
         </html>
